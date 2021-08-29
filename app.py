a4f0f2f45043aca903880f2b96d40171536ecd4c
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-08-30 03:50:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ cursor=connection.cursor()
 @app.route('/')
 def home():
      
-    cursor.execute('delete from TestyData')
+    print(connection)
     cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255) )")
     cursor.execute("show tables")
     for x in cursor:
@@ -26,7 +26,7 @@ def home():
 
 @app.route('/View')
 def View():
-    
+    print(connection)
     cursor.execute("select * from TestyData")
     data=cursor.fetchall()
     for x in data:
@@ -109,9 +109,10 @@ def predict():
         country_dic={23:'Cambodia',121:'Canada',79:'China',59:'Columbia',95:'Cuba',70:'Dominican Republic',28:'Ecuador',106:'El Salvadorr',90:'England',29:'France',137:'Germany',29:'Greece',62:'Guatemala',44:'Haiti',1:'Holand-Netherlands',13:'Honduras',24:'Hong',13:'Hungary',107:'India',43:'Iran',24:'Ireland',73:'Italy',81:'Jamaica',63:'Japan',20:'Laos',651:'Mexico',34:'Nicaragua',14:'Outlying-US(Guam-USVI-etc)',31:'Peru',220:'Philippines',60:'Poland',37:'Portugal',116:'Puerto-Rico',12:'Scotland',88:'South',35:'Taiwan',53:'Thailand',19:'Trinadad&Tobago',29663:'United States',70:'Vietnam',16:'Yugoslavia'}    
         new_contry=country_dic[df.loc[x,'country']]       
 
-
+  
     query="insert into TestyData (age,fnlwgt,education, education_num,occupation,capital_gain,capital_loss,hours_per_week,country,race,relationship,sex,workclass,prediction) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"    
     values=(int(new_age),int(new_wgt),new_ed,int(new_educationnum),new_occup,int(newcg),int(newloss),int(newhrs),new_contry,newrace,newrelation,newsex,newworkclass,output)
+    print(connection)
     cursor.execute(query,values) 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
