f5286918a8e2fe80efcd70782f9f5dcb40ddd7b7
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-08-30 02:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 app = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
-connection=sql.connect(host='us-cdbr-east-04.cleardb.com',user='b77648943f2114',password='517f5ad6',database='heroku_4fa29ab7f3558b6',connect_timeout=6000 )
+connection=sql.connect(host='us-cdbr-east-04.cleardb.com',user='b77648943f2114',password='517f5ad6',database='heroku_4fa29ab7f3558b6',connect_timeout=60000 )
 cursor=connection.cursor()
 @app.route('/')
 def home():
