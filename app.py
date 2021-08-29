bfc9fb6f77c97a67a8b9ccdef8b6a39bf2bacdaf
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-08-30 01:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
 connection=sql.connect(host='us-cdbr-east-04.cleardb.com',user='b77648943f2114',password='517f5ad6',database='heroku_4fa29ab7f3558b6',connect_timeout=6000 )
+cursor=connection.cursor()
 @app.route('/')
 def home():
     
-    cursor1=connection.cursor()
-    cursor1.execute("CREATE TABLE IF NOT EXISTS TestyData( age int, fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255) )")
-    cursor1.execute("show tables")
-    for x in cursor1:
+   
+    cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int, fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255) )")
+    cursor.execute("show tables")
+    for x in cursor:
         print(x)
     return render_template('index.html')
 
@@ -23,9 +24,9 @@ def home():
 def View():
    
  
-    cursor2=connection.cursor()
-    cursor2.execute("select * from TestyData")
-    data=cursor2.fetchall()
+    
+    cursor.execute("select * from TestyData")
+    data=cursor.fetchall()
     for x in data:
         print(x)
     return render_template('template.html',output_data=data)
@@ -108,10 +109,10 @@ def predict():
 
     
 
-    cursor3=connection.cursor()
+   
     query="insert into TestyData (age,fnlwgt,education, education_num,occupation,capital_gain,capital_loss,hours_per_week,country,race,relationship,sex,workclass,prediction) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"    
     values=(int(new_age),int(new_wgt),new_ed,int(new_educationnum),new_occup,int(newcg),int(newloss),int(newhrs),new_contry,newrace,newrelation,newsex,newworkclass,output)
-    cursor3.execute(query,values) 
+    cursor.execute(query,values) 
     return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
 
 
