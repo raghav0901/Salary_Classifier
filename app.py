caac212ac34a61ac1c62d00b72d3f428522dbe83
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-08-30 06:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,14 @@ def home():
 
     try:
       cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255) )")
+      cursor.execute("drop from TestyData")
       connection.commit()
       
     except sql.Error as err:
       connection=sql.connect(host='us-cdbr-east-04.cleardb.com',user='b77648943f2114',password='517f5ad6',database='heroku_4fa29ab7f3558b6',connect_timeout=6000)
       cursor=connection.cursor()    
       cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255) )")
+      cursor.execute("drop from TestyData")
       connection.commit()
       
     
