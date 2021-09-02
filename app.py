436f55f708ec20d3d7dc80ee982aaf4d147e03a3
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-09-03 03:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,14 +35,14 @@ def home():
     app.logger.info('Creating a Table if it already does not exist')
 
     try:
-      cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255), timing DECIMAL(3, 2) )")
+      cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255), timing DECIMAL(11, 10) )")
       connection.commit()
       
     except sql.Error as err:
       app.logger.error('Connection to the databse was lost therefore trying to connect to the database again')  
       connection=sql.connect(host='us-cdbr-east-04.cleardb.com',user='b77648943f2114',password='517f5ad6',database='heroku_4fa29ab7f3558b6',connect_timeout=6000)
       cursor=connection.cursor()    
-      cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255),timing DECIMAL(3, 2)  )")
+      cursor.execute("CREATE TABLE IF NOT EXISTS TestyData( age int , fnlwgt int, education varchar(255), education_num int, occupation varchar(255), capital_gain int, capital_loss int, hours_per_week int, country varchar(255), race varchar(255), relationship varchar(255), sex varchar(255), workclass varchar(255),prediction varchar(255),timing DECIMAL(11, 10)  )")
       connection.commit()
       
     
