2b88c83f01984eda6c36b57c3ad523ea2a931ffd
raghav0901 <89142021+raghav0901@users.noreply.github.com>
2021-09-03 01:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ connection.commit()
 
 @app.route('/')
 def home():
-    app.logger.info('Info level log')
+    app.logger.debug('this is a DEBUG message')
+    app.logger.info('this is an INFO message')
+    app.logger.warning('this is a WARNING message')
+    app.logger.error('this is an ERROR message')
+    app.logger.critical('this is a CRITICAL message')
     global connection
     global cursor
     print(connection)
